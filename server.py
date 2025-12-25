b9b1926d21297136150b5f3611528cbf9a0d209b
AngelSida <154558586+AngelSida@users.noreply.github.com>
2025-12-25 07:36:54 -0800

    Update server.py
    Added code for server

--- server.py
+++ server.py
@@ -1,1 +1,27 @@
+# server.py
+import socket, threading
+
+clients = []
+
+def relay(c):
+    while True:
+        try:
+            data = c.recv(10_000_000)
+            if not data:
+                break
+            for other in clients:
+                if other != c:
+                    other.sendall(data)
+        except:
+            break
+
+s = socket.socket()
+s.bind(("0.0.0.0", 9999))
+s.listen(2)
+print("Server running")
+
+while True:
+    c, _ = s.accept()
+    clients.append(c)
+    threading.Thread(target=relay, args=(c,), daemon=True).start()
 
